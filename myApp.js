ff04a5fde38196ec8e824c4a1b9b28e6a6639c39
devstorm321 <ddstorm0114@outlook.com>
2021-12-12 21:58:23 +0300

    static files change

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use("/public", express.static(__dirname + "/public"));
 
 console.log('Hello World');
 
